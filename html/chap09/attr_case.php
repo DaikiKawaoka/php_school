495cab3c721c936553ab0e4ffce41d55c7f985d8
DaikiKawaoka <kawadaiki@icloud.com>
2020-08-26 14:10:19 +0900

    chap9 ファイル移動

---
+++ html/chap09/attr_case.php
@@ -0,0 +1,9 @@
+<pre>
+<?php
+require_once '../DbManager.php';
+
+$db = getDb();
+$db->setAttribute(PDO::ATTR_CASE, PDO::CASE_UPPER);
+// $db->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
+// $db->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
+print_r($db->query('SELECT * FROM book')->fetchAll(PDO::FETCH_ASSOC));
